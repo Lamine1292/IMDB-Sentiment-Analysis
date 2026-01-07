d88bfcd6f5502b54d89709d212940a6361a25f77
Mouhamadou laminou <24civl5004@isik.edu.tr>
2026-01-07 22:56:01 +0300

    Initial commit - IMDB Sentiment Analysis project

---
+++ main.py
@@ -0,0 +1,6 @@
+nom= input("entrez le nom de la personne  ")
+age= input("entrez le age de la personne  ")
+
+age_prochaine= int(age)+1
+print("Vous vous appelez " + nom+ ", Vous avez "+ str(age) + " ans." )
+print( "Et l'année prochaine Vouz aurez " +str(age_prochaine) + " ans Icha Allah.")
---
+++ wordcloud-1.9.4-py39h827c3e9_0/info/test/run_test.py
@@ -0,0 +1,3 @@
+print("import: 'wordcloud'")
+import wordcloud
+
